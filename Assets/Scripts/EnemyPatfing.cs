4e3ee740da527d2c49f9a71dfba223440d5b2c6b
sebas-50 <sfrancohappy@gmail.com>
2026-03-26 15:34:39 -0500

    EnemyPathfiding

---
+++ Assets/Scripts/EnemyPatfing.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.AI;
+using NavMeshPlus;
+public class EnemyPatfing : MonoBehaviour
+{
+    public GameObject[] waypoints;
+    private int currentWaypointIndex = 0;
+    private NavMeshAgent navMeshAgent;
+    public Transform myTransform;
+    void Start()
+    {
+       navMeshAgent = GetComponent<NavMeshAgent>(); 
+       myTransform = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Esto de aqui hace que cuando llegue a un waypoint, lo cambie al siguiente, para que siga la ruta forever
+        if (waypoints != null && waypoints.Length > 0)
+        {
+            GameObject currentWaypoint = waypoints[currentWaypointIndex];
+            if (currentWaypoint != null)
+            {
+                navMeshAgent.SetDestination(currentWaypoint.transform.position); //este es el que mueve al enemigo
+                
+                if (Vector2.Distance(myTransform.position, currentWaypoint.transform.position) < 0.5f)
+                {
+                    currentWaypointIndex = (currentWaypointIndex + 1) % waypoints.Length;
+                }
+            }
+        }
+        
+    }
+}
