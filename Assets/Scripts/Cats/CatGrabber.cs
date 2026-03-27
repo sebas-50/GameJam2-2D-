d9d61515bd1a60333244b23159bff489717f78e4
sebas-50 <sfrancohappy@gmail.com>
2026-03-26 21:16:18 -0500

    Reapply "Merge branch 'Olivia' into SebasCopySecurity"
    This reverts commit cc4b025eddb2b8ee947529155888b58537068dca.

--- Assets/Scripts/Cats/CatGrabber.cs
+++ Assets/Scripts/Cats/CatGrabber.cs
@@ -14,8 +14,7 @@ public class CatGrabber : MonoBehaviour
 
     private void Start()
     {
-        GetComponent<Collider2D>();
-        GetComponent<Rigidbody2D>();
+
     }
    
     public void GrabCat(Cat cat)
@@ -36,7 +35,7 @@ public class CatGrabber : MonoBehaviour
         onCatUngrabbed?.Invoke();
     }
 
-    public void DropCatTowards(Vector2 direction)
+    public void DropCatTowardsDirection(Vector2 direction)
     {
         grabbedCat.ImpulseTowards(direction.normalized, catDroppingForce);
         UngrabCat();
@@ -45,6 +44,6 @@ public class CatGrabber : MonoBehaviour
     [ContextMenu("Drop Cat")]
     public void DropCatTowardsRandomDirection()
     {
-        DropCatTowards(new Vector2(UnityEngine.Random.Range(-1f, 1f), UnityEngine.Random.Range(-1f, 1f)));
+        DropCatTowardsDirection(new Vector2(UnityEngine.Random.Range(-1f, 1f), UnityEngine.Random.Range(-1f, 1f)));
     }
 }
