db2b99d51b5f168971766f80fd6ff9fbf85312e9
milenaGalindo <90064214+milenaGalindo@users.noreply.github.com>
2026-03-27 02:04:02 -0500

    Implementacion condiciones victoria/derrota

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,16 +1,62 @@
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Timer : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField]public float timeRemainingInMinutes; // Ejemplo: 1 minuto y 30 segundos
+    [SerializeField]private string animParameters;
+    [SerializeField]private TMP_Text displayText; // O public Text timerText; para UI antigua
+    private GameManager gameManagerRef;
+    public bool isCounting;
+
+    private void Start() {
+        gameManagerRef = GameObject.Find("GameManager").GetComponent<GameManager>();
+        timeRemainingInMinutes *= 60;
+        isCounting = true;
+    }
+
+    void Update()
     {
+        if (isCounting){
+            if (timeRemainingInMinutes > 0)
+            {
+                timeRemainingInMinutes -= Time.deltaTime;
+                DisplayTime(timeRemainingInMinutes);
+            }
+            else
+            {
+                timeRemainingInMinutes = 0;
+                // Aquí puedes llamar a un evento de "Tiempo terminado"
+            }
+        }
+        
+        else
+        {
+            displayText.text = animParameters+":)";
+        }
+    }
+
+    void DisplayTime(float timeToDisplay)
+    {
+        // Evitamos que el tiempo sea negativo en la UI
+        if (timeToDisplay >= 0)
+        {
+            displayText.text = animParameters+":(";
+
+            // Calculamos minutos y segundos
+            int minutes = Mathf.FloorToInt(timeToDisplay / 60); 
+            int seconds = Mathf.FloorToInt(timeToDisplay % 60);
+
+            // Formateamos el string con ":00" para que siempre tenga dos dígitos
+            displayText.text = animParameters + string.Format("{0:00}:{1:00}", minutes, seconds);
+        }
+        else {
+            displayText.text = animParameters+":c";
+        }
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
